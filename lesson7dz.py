6a9bd994fbc61d8bc9602e9cadf132f3784414b0
Nataliia <nvasinaa@gmail.com>
2022-09-26 14:59:11 +0300

    ДЗ 7

---
+++ lesson7dz.py
@@ -0,0 +1,14 @@
+#complex
+def calc(a:complex, b:complex, operation):
+    a = complex(a)
+    b = complex(b)
+    if operation == '+':
+        return a+b
+    if operation == '-':
+        return a-b
+    if operation == '*':
+        return a*b
+    if operation == '/':
+        return a/b
+
+    
