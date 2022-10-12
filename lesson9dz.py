7c37bcb159bafad66af4e4c5a214636184cce9a8
Nataliia <nvasinaa@gmail.com>
2022-10-12 20:35:50 +0300

    дз 9

--- lesson9dz.py
+++ lesson9dz.py
@@ -8,54 +8,21 @@ def log_to_file(temp, result):
     with open('log.csv', 'a') as file:
         file.write(f'{datetime.today()}; {temp[0]}; {temp[2]};{temp[1]};{result}; \n')
 
+#2 - Перенести игру в конфеты на интерфейс телеграма
+import telebot 
 
-# вариант человек против бота c "интеллектом":
-from random import randint
+token = '5728793661:AAGWT9H1QcrBy4VE5vGck3vGqmHX1j5JGQA'
 
-def input_dat(name):
-    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
-    while x < 1 or x > 28:
-        x = int(input(f"{name}, введите корректное количество конфет: "))
-    return x
+bot = telebot (TOKEN)
 
-
-def p_print(name, k, counter, value):
-    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+@bot.message_handler(commands=['start'])
+def stsrt_answer (msg: telebot.types.Message):
+    bot.send_message (chat_id=msg.from_user.id, text = f'Вы прислали команду: {msg.text}')
 
 
-def bot_calc(value):
-    k = randint(1,29)
-    while value-k <= 28 and value > 29:
-        k = randint(1,29)
-    return k
+@bot.message_handler()
+def answer (msg: telebot.types.Message):
+    bot.send_message (chat_id=msg.from_user.id, text = f'Вы прислали: {msg.text}')
 
-player1 = input("Введите имя первого игрока: ")
-player2 = "Bot"
-value = int(input("Введите количество конфет на столе: "))
-flag = randint(0,2) # флаг очередности
-if flag:
-    print(f"Первый ходит {player1}")
-else:
-    print(f"Первый ходит {player2}")
 
-counter1 = 0 
-counter2 = 0
-
-while value > 28:
-    if flag:
-        k = input_dat(player1)
-        counter1 += k
-        value -= k
-        flag = False
-        p_print(player1, k, counter1, value)
-    else:
-        k = bot_calc(value)
-        counter2 += k
-        value -= k
-        flag = True
-        p_print(player2, k, counter2, value)
-
-if flag:
-    print(f"Выиграл {player1}")
-else:
-    print(f"Выиграл {player2}")
+bot.polling ()
